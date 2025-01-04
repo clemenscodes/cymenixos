db85232bc9f08e1acb8d27df1344ad8780415496
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-04 13:26:19 +0100

    test

--- modules/iso/default.nix
+++ modules/iso/default.nix
@@ -2,12 +2,17 @@
   inputs,
   lib,
   ...
-}: {...}: {
+}: {config, ...}: {
   imports = ["${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"];
   isoImage = {
     squashfsCompression = "gzip -Xcompression-level 1"; # Remove this for smaller image size when writing to USB
   };
   modules = {
+    users = {
+      ${config.modules.users.user} = {
+        initialPassword = lib.mkForce null;
+      };
+    };
     disk = {
       enable = lib.mkForce false;
     };
