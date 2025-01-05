dfd7f4fe250e77e483beb511aacc26950e404324
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-05 11:02:48 +0100

    test

--- modules/disk/default.nix
+++ modules/disk/default.nix
@@ -4,7 +4,16 @@
   ...
 }: {config, ...}: let
   cfg = config.modules;
-  inherit (cfg.disk) luksDisk;
+  luksCfg = {
+    type = "luks";
+    askPassword = true;
+    settings = {
+      allowDiscards = false;
+    };
+    extraFormatArgs = ["--pkbdf argon2id"];
+    extraOpenArgs = ["--timeout 60"];
+  };
+  inherit (cfg.disk) enable device luksDisk swapsize;
 in {
   imports = [inputs.disko.nixosModules.default];
   options = {
@@ -22,101 +31,99 @@ in {
           default = "luks";
           example = "root";
         };
+        swapsize = lib.mkOption {
+          type = lib.types.int;
+          description = "The size for the swapfile in G";
+          default = "16";
+          example = "64";
+        };
       };
     };
   };
-  config = lib.mkIf (cfg.enable && cfg.disk.enable) {
+  config = lib.mkIf (cfg.enable && enable) {
     disko = {
       devices = {
         disk = {
           main = {
-            device = lib.mkDefault cfg.disk.device;
+            device = lib.mkDefault device;
             type = "disk";
             content = {
               type = "gpt";
               efiGptPartitionFirst = false;
               partitions = {
-                bios = {
-                  priority = 1;
-                  type = "EF02";
-                  size = "1M";
-                  content = {
-                    type = "filesystem";
-                    format = "vfat";
-                    mountpoint = null;
-                  };
-                  hybrid = {
-                    mbrPartitionType = "0x0c";
-                    mbrBootableFlag = false;
-                  };
-                };
-                efi = {
-                  priority = 2;
-                  label = "boot";
-                  type = "EF00";
-                  size = "512M";
-                  content = {
-                    type = "filesystem";
-                    format = "vfat";
-                    mountpoint = "/boot";
-                    mountOptions = ["umask=0077"];
-                  };
-                };
+                # bios = {
+                #   priority = 1;
+                #   type = "EF02";
+                #   size = "1M";
+                #   label = "bios";
+                #   content = {
+                #     inherit (luksCfg) type askPassword settings extraFormatArgs extraOpenArgs;
+                #     name = "${luksDisk}-bios";
+                #     content = {
+                #       type = "filesystem";
+                #       format = "vfat";
+                #       mountpoint = null;
+                #     };
+                #   };
+                #   hybrid = {
+                #     mbrPartitionType = "0x0c";
+                #     mbrBootableFlag = false;
+                #   };
+                # };
+                # efi = {
+                #   priority = 2;
+                #   label = "efi";
+                #   type = "EF00";
+                #   size = "512M";
+                #   content = {
+                #     inherit (luksCfg) type askPassword settings extraFormatArgs extraOpenArgs;
+                #     name = "${luksDisk}-efi";
+                #     content = {
+                #       type = "filesystem";
+                #       format = "vfat";
+                #       mountpoint = "/boot";
+                #       mountOptions = ["umask=0077"];
+                #     };
+                #   };
+                # };
                 luks = {
                   size = "100%";
                   label = "luks";
+                  type = "8309"; # Linux LUKS partition type
                   content = {
-                    type = "luks";
                     name = luksDisk;
-                    askPassword = true;
-                    settings = {
-                      allowDiscards = false;
-                    };
+                    inherit (luksCfg) type askPassword settings extraFormatArgs extraOpenArgs;
                     content = {
-                      type = "lvm_pv";
-                      vg = "pool";
-                    };
-                  };
-                };
-              };
-            };
-          };
-        };
-        lvm_vg = {
-          pool = {
-            type = "lvm_vg";
-            lvs = {
-              ${luksDisk} = {
-                size = "100%";
-                content = {
-                  type = "btrfs";
-                  extraArgs = ["-L" "nixos" "-f"];
-                  subvolumes = {
-                    "/" = {
-                      mountpoint = "/";
-                      mountOptions = ["subvol=root" "compress=zstd" "noatime"];
-                    };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = ["subvol=home" "compress=zstd" "noatime"];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
-                    };
-                    "/persist" = {
-                      mountpoint = "/persist";
-                      mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
-                    };
-                    "/log" = {
-                      mountpoint = "/var/log";
-                      mountOptions = ["subvol=log" "compress=zstd" "noatime"];
-                    };
-                    "/swap" = {
-                      mountpoint = "/swap";
-                      swap = {
-                        swapfile = {
-                          size = "64G";
+                      type = "btrfs";
+                      extraArgs = ["-L" "nixos" "-f"];
+                      subvolumes = {
+                        "/" = {
+                          mountpoint = "/";
+                          mountOptions = ["subvol=root" "compress=zstd" "noatime"];
+                        };
+                        "/home" = {
+                          mountpoint = "/home";
+                          mountOptions = ["subvol=home" "compress=zstd" "noatime"];
+                        };
+                        "/nix" = {
+                          mountpoint = "/nix";
+                          mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
+                        };
+                        "/persist" = {
+                          mountpoint = "/persist";
+                          mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
+                        };
+                        "/log" = {
+                          mountpoint = "/var/log";
+                          mountOptions = ["subvol=log" "compress=zstd" "noatime"];
+                        };
+                        "/swap" = {
+                          mountpoint = "/swap";
+                          swap = {
+                            swapfile = {
+                              size = "${swapsize}G";
+                            };
+                          };
                         };
                       };
                     };
