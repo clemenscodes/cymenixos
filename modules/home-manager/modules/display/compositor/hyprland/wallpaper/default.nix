51af6059961717c3aa39ae025203e90545287e05
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-07 15:08:53 +0100

    test

--- modules/home-manager/modules/display/compositor/hyprland/wallpaper/default.nix
+++ modules/home-manager/modules/display/compositor/hyprland/wallpaper/default.nix
@@ -1,18 +1,30 @@
-{
-  pkgs,
-  config,
-  ...
-}:
-pkgs.writeShellScriptBin "wallpaper" ''
-  while : ; do
-    wallpapers=($(echo "$(for file in $(ls ${config.xdg.userDirs.extraConfig.XDG_WALLPAPER_DIR}); do echo $file; done)" | grep /nix))
-    echo "Currently there are ''${#wallpapers[@]} wallpapers installed"
-    for wallpaper in $(shuf -e "''${wallpapers[@]}"); do
-      echo "Setting wallpaper to $wallpaper"
-      ${pkgs.swww}/bin/swww img "$wallpaper"
-      echo "Setting random wallpaper $wallpaper to $XDG_WALLPAPER_DIR/random"
-      cp "$wallpaper" "$XDG_WALLPAPER_DIR/random"
-      sleep 3600
+{pkgs, ...}: let
+  wallpapers = pkgs.stdenv.mkDerivation {
+    name = "wallpapers";
+    src = pkgs.fetchFromGitHub {
+      owner = "orangci";
+      repo = "walls";
+      rev = "886dd0786cea003f9e94a1054137e7cbd8fd7428";
+      hash = "sha256-GjP7ASUjfL9kqIx+/dhGkiBm1QmjwWn80bqhi3Vp6vM=";
+    };
+    installPhase = ''
+      mkdir -p $out
+      cp -r $src/* $out
+      rm $out/README.md
+    '';
+  };
+in
+  pkgs.writeShellScriptBin "wallpaper" ''
+    while : ; do
+      wallpapers=($(echo "$(for file in $(ls ${wallpapers}); do echo $file; done)" | grep /nix))
+      echo "Currently there are ''${#wallpapers[@]} wallpapers installed"
+      for wallpaper in $(shuf -e "''${wallpapers[@]}"); do
+        echo "Setting wallpaper to $wallpaper"
+        ${pkgs.swww}/bin/swww img "$wallpaper"
+        echo "Setting random wallpaper $wallpaper to $XDG_WALLPAPER_DIR/random"
+        rm "$XDG_WALLPAPER_DIR/random"
+        cp "$wallpaper" "$XDG_WALLPAPER_DIR/random"
+        sleep 3600
+      done
     done
-  done
-''
+  ''
