ec11ecb565d79581938cf26bb8e89a497f49301b
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-07 14:10:20 +0100

    feat(wallpaper): shuffle wallpapers each time

--- modules/home-manager/modules/display/compositor/hyprland/wallpaper/default.nix
+++ modules/home-manager/modules/display/compositor/hyprland/wallpaper/default.nix
@@ -1,8 +1,7 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "wallpaper" ''
-  sleep 0.4
   while : ; do
-    for file in $(echo "$(for file in $(ls $XDG_WALLPAPER_DIR); do echo $file; done)" | grep /nix); do
+    for file in $(echo "$(for file in $(ls $XDG_WALLPAPER_DIR); do echo $file; done)" | grep /nix | shuf); do
        echo "Setting wallpaper to $file"
        ${pkgs.swww}/bin/swww img "$file"
        sleep 3600
