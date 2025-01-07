64b032d31263158ccf8eb6c5f572528c05233979
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-07 14:47:29 +0100

    test

--- modules/home-manager/modules/display/compositor/hyprland/wallpaper/default.nix
+++ modules/home-manager/modules/display/compositor/hyprland/wallpaper/default.nix
@@ -2,12 +2,12 @@
 pkgs.writeShellScriptBin "wallpaper" ''
   while : ; do
     wallpapers=$(echo "$(for file in $(ls $XDG_WALLPAPER_DIR); do echo $file; done)" | grep /nix | shuf)
-    for file in "$wallpapers"; do
-      random_wallpaper=$(echo $wallpapers | tail -n1)
-      echo "Setting random wallpaper $random_wallpaper to $XDG_WALLPAPER_DIR/random"
-      cp $random_wallpaper $XDG_WALLPAPER_DIR/random
-      echo "Setting wallpaper to $file"
-      ${pkgs.swww}/bin/swww img "$file"
+    echo "Currently there are $(echo "$wallpapers" | wc -l) installed"
+    for wallpaper in "$wallpapers"; do
+      echo "Setting wallpaper to $wallpaper"
+      ${pkgs.swww}/bin/swww img "$wallpaper"
+      echo "Setting random wallpaper $wallpaper to $XDG_WALLPAPER_DIR/random"
+      cp "$wallpaper" "$XDG_WALLPAPER_DIR/random"
       sleep 3600
     done
   done
