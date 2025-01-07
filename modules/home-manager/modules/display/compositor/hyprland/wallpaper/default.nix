156c64bb6fb46bb14308da6292907e1a10ed8c14
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-07 13:47:25 +0100

    update wallpaper script

--- modules/home-manager/modules/display/compositor/hyprland/wallpaper/default.nix
+++ modules/home-manager/modules/display/compositor/hyprland/wallpaper/default.nix
@@ -1,11 +1,13 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "wallpaper" ''
   sleep 0.4
-  ${pkgs.swww}/bin/swww-daemon -q && \
-    while : ; do
-       for file in $(ls $XDG_WALLPAPER_DIR/*); do
-         swww img "$file"
-         sleep 3600
-       done
+  ${pkgs.swww}/bin/swww-daemon
+  echo "Wallpaper daemon started..."
+  while : ; do
+    for file in $(echo "$(for file in $(ls $XDG_WALLPAPER_DIR); do echo $file; done)" | grep /nix); do
+       echo "Setting wallpaper to $file"
+       ${pkgs.swww}/bin/swww img "$file"
+       sleep 3600
     done
+  done
 ''
