a41682890472ca6e37712d33833424f3617e6b2f
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-07 15:56:47 +0100

    test

--- modules/home-manager/modules/display/compositor/hyprland/random-wallpaper/default.nix
+++ modules/home-manager/modules/display/compositor/hyprland/random-wallpaper/default.nix
@@ -1,8 +1,7 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "random-wallpaper" ''
   wallpaper="$(echo "$(for file in $(ls "$XDG_WALLPAPER_DIR"); do echo $file; done)" | shuf | head -n1)"
-  echo "Setting random wallpaper $wallpaper to $XDG_WALLPAPER_DIR/random"
   ln -sf "$wallpaper" "$XDG_WALLPAPER_DIR/random"
-  echo "Setting wallpaper to $wallpaper"
   ${pkgs.swww}/bin/swww img "$XDG_WALLPAPER_DIR/random"
+  echo "Setting wallpaper to $wallpaper"
 ''
