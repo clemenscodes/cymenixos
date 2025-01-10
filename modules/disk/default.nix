3f276271bb6ad71faf09aec3434a3031c00da239
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-10 11:11:05 +0100

    test

--- modules/disk/default.nix
+++ modules/disk/default.nix
@@ -43,7 +43,7 @@ in {
               type = "gpt";
               efiGptPartitionFirst = false;
               partitions = {
-                bios = lib.mkIf cfg.modules.boot.efiSupport {
+                bios = lib.mkIf cfg.boot.efiSupport {
                   priority = 1;
                   type = "EF02";
                   size = "1M";
@@ -58,7 +58,7 @@ in {
                     mbrBootableFlag = false;
                   };
                 };
-                efi = lib.mkIf cfg.modules.boot.efiSupport {
+                efi = lib.mkIf cfg.boot.efiSupport {
                   priority = 2;
                   label = "efi";
                   type = "EF00";
