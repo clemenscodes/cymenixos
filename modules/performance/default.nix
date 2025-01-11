65eec2fe3c4522d2b7aefdd85ca359d94aa84553
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-11 09:41:38 +0100

    test

--- modules/performance/default.nix
+++ modules/performance/default.nix
@@ -18,9 +18,20 @@
     };
   };
   config = lib.mkIf (config.modules.enable && config.modules.performance.enable) {
-    services = {
-      getty = {
-        enable = false;
+    systemd = {
+      services = {
+        "getty@tty1" = {
+          enable = false;
+        };
+        "autovt@tty1" = {
+          enable = false;
+        };
+        "getty@tty7" = {
+          enable = false;
+        };
+        "autovt@tty7" = {
+          enable = false;
+        };
       };
     };
   };
