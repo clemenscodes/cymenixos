3365607b4b42048cd07f2a1a71e78ea91a1db470
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-12 14:03:39 +0100

    fix(element): remove duplicated desktop entry

--- modules/home-manager/modules/media/communication/element/default.nix
+++ modules/home-manager/modules/media/communication/element/default.nix
@@ -9,11 +9,6 @@
 }: let
   cfg = config.modules.media.communication;
   isDesktop = osConfig.modules.display.gui != "headless";
-  element = pkgs.element-desktop;
-  elementIcon = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/element-hq/element-desktop/refs/heads/develop/res/img/element.png";
-    sha256 = "sha256-FDq3fPk6imYMWxJJnJPH+gBAwBklv+DjHEc42mqmgoU=";
-  };
 in {
   options = {
     modules = {
@@ -28,23 +23,10 @@ in {
   };
   config = lib.mkIf (cfg.enable && cfg.element.enable && isDesktop) {
     home = {
+      packages = [pkgs.element];
       persistence = {
         "${osConfig.modules.boot.impermanence.persistPath}${config.home.homeDirectory}" = {
           directories = [".config/Element"];
-        };
-      };
-      packages = [element];
-    };
-    xdg = {
-      desktopEntries = {
-        element = {
-          name = "Element";
-          type = "Application";
-          categories = ["Network" "InstantMessaging"];
-          exec = "${element}/bin/element-desktop";
-          genericName = "A feature-rich client for Matrix.org";
-          icon = "${elementIcon}";
-          mimeType = ["x-scheme-handler/element"];
         };
       };
     };
