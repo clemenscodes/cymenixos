5f18ad80dfa4f8ae48faa258902565f83b9311f9
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-12 16:36:40 +0100

    test

--- modules/home-manager/modules/explorer/yazi/plugins/smart-enter.nix
+++ modules/home-manager/modules/explorer/yazi/plugins/smart-enter.nix
@@ -1,10 +1,19 @@
-{pkgs, ...}:
-pkgs.writeText "smart-enter" ''
-  --- @sync entry
-  return {
-  	entry = function()
-  		local h = cx.active.current.hovered
-  		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
-  	end,
+{pkgs, ...}: let
+  initLua = pkgs.writeText "smart-enter-lua" ''
+    --- @sync entry
+    return {
+    	entry = function()
+    		local h = cx.active.current.hovered
+    		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+    	end,
+    }
+  '';
+in
+  pkgs.stdenv.mkDerivation {
+    name = "smart-enter";
+    phases = "installPhase";
+    installPhase = ''
+      mkdir -p $out
+      ln -sf ${initLua} $out/init.lua
+    '';
   }
-''
--- modules/home-manager/modules/explorer/yazi/plugins/smart-tab.nix
+++ modules/home-manager/modules/explorer/yazi/plugins/smart-tab.nix
@@ -1,10 +1,19 @@
-{pkgs, ...}:
-pkgs.writeText "smart-tab" ''
-  --- @sync entry
-  return {
-  	entry = function()
-  		local h = cx.active.current.hovered
-  		ya.manager_emit("tab_create", h and h.cha.is_dir and { h.url } or { current = true })
-  	end,
+{pkgs, ...}: let
+  initLua = pkgs.writeText "smart-tab-init-lua" ''
+    --- @sync entry
+    return {
+    	entry = function()
+    		local h = cx.active.current.hovered
+    		ya.manager_emit("tab_create", h and h.cha.is_dir and { h.url } or { current = true })
+    	end,
+    }
+  '';
+in
+  pkgs.stdenv.mkDerivation {
+    name = "smart-tab";
+    phases = "installPhase";
+    installPhase = ''
+      mkdir -p $out
+      ln -sf ${initLua} $out/init.lua
+    '';
   }
-''
