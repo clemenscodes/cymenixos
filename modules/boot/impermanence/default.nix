2c658d219db45c438076f221dd36d1a3a65f6c58
Clemens Horn <clemens.horn@mni.thm.de>
2025-01-31 10:15:11 +0100

    fix(btrfs): no root snapshots, boot persist logs

--- modules/boot/impermanence/default.nix
+++ modules/boot/impermanence/default.nix
@@ -33,25 +33,6 @@ in {
         postDeviceCommands = lib.mkAfter ''
           mkdir -p /btrfs_tmp
           mount /dev/${config.modules.disk.vg}/${config.modules.disk.lvm_volume} /btrfs_tmp
-
-          if [[ -e /btrfs_tmp/root ]]; then
-            mkdir -p /btrfs_tmp/snapshots/roots
-            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-            mv /btrfs_tmp/root "/btrfs_tmp/snapshots/roots/$timestamp"
-          fi
-
-          delete_subvolume_recursively() {
-            IFS=$'\n'
-            for subvolume in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "/btrfs_tmp/snapshots/roots/$subvolume"
-            done
-            btrfs subvolume delete "$1"
-          }
-
-          for subvolume in $(find /btrfs_tmp/snapshots/roots -maxdepth 1 -mtime +7); do
-            delete_subvolume_recursively "$subvolume"
-          done
-
           btrfs subvolume create /btrfs_tmp/root
           umount /btrfs_tmp
         '';
--- modules/disk/default.nix
+++ modules/disk/default.nix
@@ -246,7 +246,6 @@ in {
           "d /snapshots/persist 0755 root root"
           "d /snapshots/logs 0755 root root"
           "d /snapshots/boots 0755 root root"
-          "d /snapshots/roots 0755 root root"
         ];
       };
     };
