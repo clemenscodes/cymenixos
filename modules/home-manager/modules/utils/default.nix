f7f488446d83a48ad166bd40ee664c46c86e690f
Clemens Horn <clemens.horn@mni.thm.de>
2025-02-03 15:37:51 +0100

    fix(nix): import module

--- modules/home-manager/modules/utils/default.nix
+++ modules/home-manager/modules/utils/default.nix
@@ -14,6 +14,7 @@
     (import ./ripgrep {inherit inputs pkgs lib;})
     (import ./tldr {inherit inputs pkgs lib;})
     (import ./unzip {inherit inputs pkgs lib;})
+    (import ./wget {inherit inputs pkgs lib;})
     (import ./zip {inherit inputs pkgs lib;})
   ];
   options = {
