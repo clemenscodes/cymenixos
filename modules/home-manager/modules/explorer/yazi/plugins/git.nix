d29d52ac7afba19ed0081a67bf2533fa2eae8901
Clemens Horn <clemens.horn@mni.thm.de>
2025-02-09 13:29:01 +0100

    chore(nix): update more plugins

--- modules/home-manager/modules/explorer/yazi/plugins/git.nix
+++ modules/home-manager/modules/explorer/yazi/plugins/git.nix
@@ -1,16 +1,13 @@
-{pkgs, ...}: let
-  plugins = pkgs.fetchFromGitHub {
-    owner = "yazi-rs";
-    repo = "plugins";
-    rev = "07258518f3bffe28d87977bc3e8a88e4b825291b";
-    hash = "sha256-axoMrOl0pdlyRgckFi4DiS+yBKAIHDhVeZQJINh8+wk=";
-  };
-in
-  pkgs.stdenv.mkDerivation {
-    name = "git";
-    phases = "installPhase";
-    installPhase = ''
-      mkdir -p $out
-      ln -sf ${plugins}/git.yazi/* $out
-    '';
-  }
+{
+  pkgs,
+  plugins,
+  ...
+}:
+pkgs.stdenv.mkDerivation {
+  name = "git";
+  phases = "installPhase";
+  installPhase = ''
+    mkdir -p $out
+    ln -sf ${plugins}/git.yazi/* $out
+  '';
+}
