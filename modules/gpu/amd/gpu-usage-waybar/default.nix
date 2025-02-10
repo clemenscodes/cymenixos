df2cf69aed4f8ac66b51acd532eca11d5720b665
Clemens Horn <clemens.horn@mni.thm.de>
2025-02-10 09:04:03 +0100

    fix sha

--- modules/gpu/amd/gpu-usage-waybar/default.nix
+++ modules/gpu/amd/gpu-usage-waybar/default.nix
@@ -6,7 +6,7 @@ pkgs.rustPlatform.buildRustPackage rec {
     owner = "cymenix";
     repo = pname;
     rev = "b92311709f29a28c3f4f52172ae82d7ea34d1eac";
-    hash = "sha256-NY6Xv2yg1QJO7enzJjtYXb/Zd1pNAtr0wENDQwerJ5w=";
+    hash = "sha256-Q3AtM5fCtmPRYTIRKDW+ROpS0TV0jpnw8XtQNO5JSpo=";
   };
   cargoHash = "sha256-TOV+0cB2OmtsixBBLGYod1YJckx9a2Ar+uJWcKGqM/0=";
 }
