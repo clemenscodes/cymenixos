3eaa4d81220e038fcad0ed8241931819e12da82e
Clemens Horn <clemens.horn@mni.thm.de>
2025-02-21 16:33:06 +0100

    test

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -46,54 +46,34 @@ in {
   };
   config = lib.mkIf (cfg.enable && cfg.boot.enable) {
     boot = {
-      kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
-      kernelModules = ["tcp_bpr" "v4l2loopback"];
-      kernel = {
-        sysctl = {
-          # The Magic SysRq key is a key combo that allows users connected to the
-          # system console of a Linux kernel to perform some low-level commands.
-          # Disable it, since we don't need it, and is a potential security concern.
-          "kernel.sysrq" = 0;
-
-          ## TCP hardening
-          # Prevent bogus ICMP errors from filling up logs.
-          "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
-          # Reverse path filtering causes the kernel to do source validation of
-          # packets received from all interfaces. This can mitigate IP spoofing.
-          "net.ipv4.conf.default.rp_filter" = 1;
-          "net.ipv4.conf.all.rp_filter" = 1;
-          # Do not accept IP source route packets (we're not a router)
-          "net.ipv4.conf.all.accept_source_route" = 0;
-          "net.ipv6.conf.all.accept_source_route" = 0;
-          # Don't send ICMP redirects (again, we're not a router)
-          "net.ipv4.conf.all.send_redirects" = 0;
-          "net.ipv4.conf.default.send_redirects" = 0;
-          # Refuse ICMP redirects (MITM mitigations)
-          "net.ipv4.conf.all.accept_redirects" = 0;
-          "net.ipv4.conf.default.accept_redirects" = 0;
-          "net.ipv4.conf.all.secure_redirects" = 0;
-          "net.ipv4.conf.default.secure_redirects" = 0;
-          "net.ipv6.conf.all.accept_redirects" = 0;
-          "net.ipv6.conf.default.accept_redirects" = 0;
-          # Protects against SYN flood attacks
-          "net.ipv4.tcp_syncookies" = 1;
-          # Incomplete protection again TIME-WAIT assassination
-          "net.ipv4.tcp_rfc1337" = 1;
-
-          ## TCP optimization
-          # TCP Fast Open is a TCP extension that reduces network latency by packing
-          # data in the sender’s initial TCP SYN. Setting 3 = enable TCP Fast Open for
-          # both incoming and outgoing connections:
-          "net.ipv4.tcp_fastopen" = 3;
-          # Bufferbloat mitigations + slight improvement in throughput & latency
-          "net.ipv4.tcp_congestion_control" = "bbr";
-          "net.core.default_qdisc" = "cake";
-        };
-      };
       supportedFilesystems = lib.mkForce ["btrfs" "vfat" "reiserfs" "f2fs" "xfs" "ntfs" "cifs"];
-      tmp = {
-        useTmpfs = lib.mkDefault true;
-        cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
+      kernelModules = ["tcp_bpr" "v4l2loopback"];
+      kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+      kernelParams = lib.mkIf hibernation ["resume_offset=${builtins.toString swapResumeOffset}"];
+      resumeDevice = lib.mkIf hibernation "/dev/disk/by-label/nixos";
+      consoleLogLevel = lib.mkDefault 0;
+      extraModulePackages = [config.boot.kernelPackages.v4l2loopback.out];
+      extraModprobeConfig = ''
+        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+      '';
+      initrd = {
+        verbose = false;
+        availableKernelModules = [
+          "ohci_pci"
+          "ohci_hcd"
+          "ehci_pci"
+          "ehci_hcd"
+          "xhci_pci"
+          "xhci_hcd"
+          "uas"
+          "usb_storage"
+          "usbhid"
+          "ahci"
+          "nvme"
+          "sr_mod"
+          "aesni_intel"
+          "cryptd"
+        ];
       };
       loader = lib.mkIf (!cfg.boot.secureboot.enable) {
         efi = {
@@ -116,46 +96,12 @@ in {
             "grub/${pkgs.grub2_efi.grubTarget}/argon2.mod" = lib.mkIf efiSupport "${pkgs.grub2_efi}/lib/grub/${pkgs.grub2_efi.grubTarget}/argon2.mod";
             "grub/${pkgs.grub2_efi.grubTarget}/argon2.module" = lib.mkIf efiSupport "${pkgs.grub2_efi}/lib/grub/${pkgs.grub2_efi.grubTarget}/argon2.module";
           };
-          mirroredBoots = lib.mkForce [
-            {
-              path = "/boot";
-              devices = [
-                (lib.mkIf (biosSupport || libreboot) device)
-                (lib.mkIf efiSupport "nodev")
-              ];
-              inherit (config.boot.loader.efi) efiSysMountPoint;
-            }
-          ];
         };
       };
-      kernelParams = lib.mkIf hibernation [
-        "resume_offset=${builtins.toString swapResumeOffset}"
-      ];
-      resumeDevice = lib.mkIf hibernation "/dev/disk/by-label/nixos";
-      consoleLogLevel = lib.mkDefault 0;
-      initrd = {
-        verbose = false;
-        availableKernelModules = [
-          "ohci_pci"
-          "ohci_hcd"
-          "ehci_pci"
-          "ehci_hcd"
-          "xhci_pci"
-          "xhci_hcd"
-          "uas"
-          "usb_storage"
-          "usbhid"
-          "ahci"
-          "nvme"
-          "sr_mod"
-          "aesni_intel"
-          "cryptd"
-        ];
+      tmp = {
+        useTmpfs = lib.mkDefault true;
+        cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
       };
-      extraModulePackages = [config.boot.kernelPackages.v4l2loopback.out];
-      extraModprobeConfig = ''
-        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-      '';
     };
   };
 }
