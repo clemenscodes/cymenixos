99a25f111db4c47ec5925bb949df0dbd84766002
Clemens Horn <clemens.horn@mni.thm.de>
2025-02-26 14:47:49 +0100

    test

--- modules/iso/make-squashfs.nix
+++ modules/iso/make-squashfs.nix
@@ -62,7 +62,7 @@ in
         # Generate the squashfs image.
         mksquashfs nix-path-registration $out/closure $imgPath ${pseudoFilesArgs} \
           -no-hardlinks ${lib.optionalString noStrip "-no-strip"} -keep-as-directory -all-root -b 1048576 ${compFlag} \
-          -processors $NIX_BUILD_CORES -root-mode 0755 -info -progress -no-exports -mem 8GB
+          -processors $NIX_BUILD_CORES -root-mode 0755 -info -progress
       ''
       + lib.optionalString hydraBuildProduct ''
 
