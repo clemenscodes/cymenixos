3348cc9ceb5de214601cc49fe2835ea5f9be8cfa
Clemens Horn <clemens.horn@mni.thm.de>
2025-03-07 11:33:49 +0100

    feat(zotero): persist settings

--- modules/home-manager/modules/organization/zotero/default.nix
+++ modules/home-manager/modules/organization/zotero/default.nix
@@ -22,6 +22,14 @@ in {
   config = lib.mkIf (cfg.enable && cfg.zotero.enable && isDesktop) {
     home = {
       packages = [pkgs.zotero];
+      persistence = {
+        "${osConfig.modules.boot.impermanence.persistPath}${config.home.homeDirectory}" = {
+          directories = [
+            ".cache/zotero"
+            ".zotero"
+          ];
+        };
+      };
     };
   };
 }
