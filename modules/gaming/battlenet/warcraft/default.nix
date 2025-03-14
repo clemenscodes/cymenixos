3bd01caa8df5906725058fb1d12afac9bea0e2b9
Clemens Horn <clemens.horn@mni.thm.de>
2025-03-14 20:22:31 +0100

    test

--- modules/gaming/battlenet/warcraft/default.nix
+++ modules/gaming/battlenet/warcraft/default.nix
@@ -47,6 +47,42 @@
       hyprctl dispatch submap reset
     '';
   };
+  warcraft-chat-open = pkgs.writeShellApplication {
+    name = "warcraft-chat-open";
+    runtimeInputs = [
+      pkgs.hyprland
+      pkgs.systemd
+    ];
+    text = ''
+      hyprctl dispatch submap CHAT
+      systemctl --user stop xremap-warcraft.service
+      systemctl --user start xremap.service
+    '';
+  };
+  warcraft-chat-send = pkgs.writeShellApplication {
+    name = "warcraft-chat-send";
+    runtimeInputs = [
+      pkgs.systemd
+      pkgs.hyprland
+    ];
+    text = ''
+      systemctl --user stop xremap.service
+      systemctl --user start xremap-warcraft.service
+      hyprctl dispatch submap WARCRAFT
+    '';
+  };
+  warcraft-chat-close = pkgs.writeShellApplication {
+    name = "warcraft-chat-close";
+    runtimeInputs = [
+      pkgs.systemd
+      pkgs.hyprland
+    ];
+    text = ''
+      systemctl --user stop xremap.service
+      systemctl --user start xremap-warcraft.service
+      hyprctl dispatch submap WARCRAFT
+    '';
+  };
   warcraft-autocast-hotkey = pkgs.writeShellApplication {
     name = "warcraft-autocast-hotkey";
     runtimeInputs = [
@@ -175,102 +211,11 @@
       ydotool mousemove --xpos "$MOUSE_X" --ypos "$MOUSE_Y"
     '';
   };
-  warcraft-chat-open = pkgs.writeShellApplication {
-    name = "warcraft-chat-open";
-    runtimeInputs = [
-      pkgs.ydotool
-      pkgs.hyprland
-      pkgs.systemd
-    ];
-    text = ''
-      YDOTOOL_LOG_FILE="$WARCRAFT_HOME/ydotool_log"
-
-      echo "Opening warcraft chat" >> "$YDOTOOL_LOG_FILE"
-
-      systemctl --user stop xremap-warcraft.service
-      systemctl --user start xremap.service
-      ydotool key 96:1 96:0 # Press Numpad_Enter
-      hyprctl dispatch submap CHAT
-    '';
-  };
-  warcraft-chat-send = pkgs.writeShellApplication {
-    name = "warcraft-chat-send";
-    runtimeInputs = [
-      pkgs.systemd
-      pkgs.ydotool
-      pkgs.hyprland
-    ];
-    text = ''
-      YDOTOOL_LOG_FILE="$WARCRAFT_HOME/ydotool_log"
-
-      echo "Sending warcraft chat" >> "$YDOTOOL_LOG_FILE"
-
-      systemctl --user stop xremap.service
-      systemctl --user start xremap-warcraft.service
-      ydotool key 96:1 96:0 # Press Numpad_Enter
-      hyprctl dispatch submap WARCRAFT
-    '';
-  };
-  warcraft-chat-close = pkgs.writeShellApplication {
-    name = "warcraft-chat-close";
-    runtimeInputs = [
-      pkgs.systemd
-      pkgs.ydotool
-      pkgs.hyprland
-    ];
-    text = ''
-      YDOTOOL_LOG_FILE="$WARCRAFT_HOME/ydotool_log"
-
-      echo "Closing warcraft chat" >> "$YDOTOOL_LOG_FILE"
-
-      systemctl --user stop xremap.service
-      systemctl --user start xremap-warcraft.service
-      ydotool key 58:1 58:0 # Press caps lock which is actually escape
-      ydotool key 1:1 1:0 # Press escape again to be sure
-      hyprctl dispatch submap WARCRAFT
-    '';
-  };
   warcraft-write-control-group = pkgs.writeShellApplication {
     name = "warcraft-write-control-group";
     excludeShellChecks = ["SC2046" "SC2086"];
     text = ''
       echo "$1" > "$WARCRAFT_HOME/control_group"
-    '';
-  };
-  warcraft-create-control-group = pkgs.writeShellApplication {
-    name = "warcraft-create-control-group";
-    excludeShellChecks = ["SC2046" "SC2086"];
-    runtimeInputs = [
-      pkgs.ydotool
-      pkgs.hyprland
-    ];
-    text = ''
-      hyprctl dispatch submap CONTROLGROUP
-
-      YDOTOOL_LOG_FILE="$WARCRAFT_HOME/ydotool_log"
-      CONTROL_GROUP_FILE="$WARCRAFT_HOME/control_group"
-      CONTROL_GROUP="$1"
-
-      echo "$CONTROL_GROUP" > "$CONTROL_GROUP_FILE"
-
-      echo "Creating control group $CONTROL_GROUP" >> "$YDOTOOL_LOG_FILE"
-
-      case "$CONTROL_GROUP" in
-        1) CONTROL_GROUP_KEYCODE=2 ;;
-        2) CONTROL_GROUP_KEYCODE=3 ;;
-        3) CONTROL_GROUP_KEYCODE=4 ;;
-        4) CONTROL_GROUP_KEYCODE=5 ;;
-        5) CONTROL_GROUP_KEYCODE=6 ;;
-        6) CONTROL_GROUP_KEYCODE=7 ;;
-        7) CONTROL_GROUP_KEYCODE=8 ;;
-        8) CONTROL_GROUP_KEYCODE=9 ;;
-        9) CONTROL_GROUP_KEYCODE=10 ;;
-        0) CONTROL_GROUP_KEYCODE=11 ;;
-      esac
-
-      ydotool key 57:1 "$CONTROL_GROUP_KEYCODE":1 "$CONTROL_GROUP_KEYCODE":0 57:0
-
-      hyprctl dispatch submap WARCRAFT
     '';
   };
   warcraft-edit-unit-control-group = pkgs.writeShellApplication {
@@ -281,8 +226,6 @@
     ];
     excludeShellChecks = ["SC2046" "SC2086"];
     text = ''
-      hyprctl dispatch submap CONTROLGROUP
-
       YDOTOOL_LOG_FILE="$WARCRAFT_HOME/ydotool_log"
       CONTROL_GROUP_FILE="$WARCRAFT_HOME/control_group"
       CONTROL_GROUP="$(cat "$CONTROL_GROUP_FILE")"
@@ -302,14 +245,7 @@
 
       echo "Removing unit from control group $CONTROL_GROUP" >> "$YDOTOOL_LOG_FILE"
 
-      sleep 0.1
-
-      ydotool key 42:1
-      ydotool click 0xC0
-      ydotool key 42:0
       ydotool key 57:1 "$CONTROL_GROUP_KEYCODE":1 "$CONTROL_GROUP_KEYCODE":0 57:0
-
-      hyprctl dispatch submap WARCRAFT
     '';
   };
   warcraft-select-unit = pkgs.writeShellApplication {
@@ -386,13 +322,12 @@
       warcraft
       warcraft-mode-start
       warcraft-mode-stop
-      warcraft-autocast-hotkey
-      warcraft-inventory-hotkey
       warcraft-chat-open
       warcraft-chat-send
       warcraft-chat-close
+      warcraft-autocast-hotkey
+      warcraft-inventory-hotkey
       warcraft-write-control-group
-      warcraft-create-control-group
       warcraft-edit-unit-control-group
       warcraft-select-unit
     ];
@@ -446,7 +381,6 @@ in {
                   bind = CTRL, W, exec, ${lib.getExe warcraft-mode-start}
                   submap = WARCRAFT
                   bind = ALT, W, exec, ${lib.getExe warcraft-mode-stop}
-                  bind = SHIFT, mouse:272, exec, ${lib.getExe warcraft-edit-unit-control-group}
                   bind = SHIFT, Q, exec, ${lib.getExe warcraft-autocast-hotkey} Q
                   bind = SHIFT, W, exec, ${lib.getExe warcraft-autocast-hotkey} W
                   bind = SHIFT, E, exec, ${lib.getExe warcraft-autocast-hotkey} E
@@ -465,31 +399,18 @@ in {
                   bind = CTRL, S, exec, ${lib.getExe warcraft-inventory-hotkey} 4
                   bind = CTRL, Y, exec, ${lib.getExe warcraft-inventory-hotkey} 5
                   bind = CTRL, X, exec, ${lib.getExe warcraft-inventory-hotkey} 6
-                  bind = , U, exec, ${lib.getExe warcraft-write-control-group} 1
-                  bind = , I, exec, ${lib.getExe warcraft-write-control-group} 2
-                  bind = , O, exec, ${lib.getExe warcraft-write-control-group} 3
-                  bind = , P, exec, ${lib.getExe warcraft-write-control-group} 4
-                  bind = , H, exec, ${lib.getExe warcraft-write-control-group} 5
-                  bind = , J, exec, ${lib.getExe warcraft-write-control-group} 6
-                  bind = , K, exec, ${lib.getExe warcraft-write-control-group} 7
-                  bind = , L, exec, ${lib.getExe warcraft-write-control-group} 8
-                  bind = , N, exec, ${lib.getExe warcraft-write-control-group} 9
-                  bind = , M, exec, ${lib.getExe warcraft-write-control-group} 0
-                  bind = CTRL, Control_L, submap, CTRL
-                  submap = CTRL
-                  bind = , 1, exec, ${lib.getExe warcraft-create-control-group} 1
-                  bind = , 2, exec, ${lib.getExe warcraft-create-control-group} 2
-                  bind = , 3, exec, ${lib.getExe warcraft-create-control-group} 3
-                  bind = , 4, exec, ${lib.getExe warcraft-create-control-group} 4
-                  bind = , 5, exec, ${lib.getExe warcraft-create-control-group} 5
-                  bind = , 6, exec, ${lib.getExe warcraft-create-control-group} 6
-                  bind = , 7, exec, ${lib.getExe warcraft-create-control-group} 7
-                  bind = , 8, exec, ${lib.getExe warcraft-create-control-group} 8
-                  bind = , 9, exec, ${lib.getExe warcraft-create-control-group} 9
-                  bind = , 0, exec, ${lib.getExe warcraft-create-control-group} 0
-                  bind = , catchall, submap, WARCRAFT
-                  submap = WARCRAFT
-                  bind = , RETURN, exec, ${lib.getExe warcraft-chat-open}
+                  bindn = , RETURN, exec, ${lib.getExe warcraft-chat-open}
+                  bindn = SHIFT, mouse:272, exec, ${lib.getExe warcraft-edit-unit-control-group}
+                  bindn = , 1, exec, ${lib.getExe warcraft-write-control-group} 1
+                  bindn = , 2, exec, ${lib.getExe warcraft-write-control-group} 2
+                  bindn = , 3, exec, ${lib.getExe warcraft-write-control-group} 3
+                  bindn = , 4, exec, ${lib.getExe warcraft-write-control-group} 4
+                  bindn = , 5, exec, ${lib.getExe warcraft-write-control-group} 5
+                  bindn = , 6, exec, ${lib.getExe warcraft-write-control-group} 6
+                  bindn = , 7, exec, ${lib.getExe warcraft-write-control-group} 7
+                  bindn = , 8, exec, ${lib.getExe warcraft-write-control-group} 8
+                  bindn = , 9, exec, ${lib.getExe warcraft-write-control-group} 9
+                  bindn = , 0, exec, ${lib.getExe warcraft-write-control-group} 0
                   bind = , mouse:276, submap, BTN_EXTRA
                   bind = , mouse:275, submap, BTN_SIDE
                   binde = , XF86AudioRaiseVolume, exec, ${pkgs.wireplumber}/bin/wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+
@@ -522,13 +443,9 @@ in {
                   bind = , F, submap, WARCRAFT
                   bind = , G, submap, WARCRAFT
                   bind = , catchall, submap, WARCRAFT
-                  submap = CONTROLGROUP
-                  bind = , catchall, submap, WARCRAFT
-                  bind = $mod, Q, submap, WARCRAFT
-                  bind = $mod SHIFT, Q, submap, reset
                   submap = CHAT
-                  bind = , RETURN, exec, ${lib.getExe warcraft-chat-send}
-                  bind = , ESCAPE, exec, ${lib.getExe warcraft-chat-close}
+                  bindn = , RETURN, exec, ${lib.getExe warcraft-chat-send}
+                  bindn = , ESCAPE, exec, ${lib.getExe warcraft-chat-close}
                   submap = reset
                 '';
               };
