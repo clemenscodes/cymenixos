a827b49ccb920153cc24e5fc6815e121858c660f
Clemens Horn <clemens.horn@mni.thm.de>
2025-03-18 19:28:39 +0100

    test

--- modules/gaming/lutris/default.nix
+++ modules/gaming/lutris/default.nix
@@ -27,11 +27,12 @@ in {
         (pkgs.lutris.override {
           extraPkgs = pkgs: [
             pkgs.winetricks
-            # pkgs."wine-wow64-bleeding-9.22"
-            # pkgs."wine-wow64-bleeding-winetricks-9.22"
+            pkgs."wine-wow64-bleeding-9.22"
+            pkgs."wine-wow64-bleeding-winetricks-9.22"
             # pkgs."wine-wow64-bleeding-10.3"
             # pkgs."wine-wow64-bleeding-winetricks-10.3"
           ];
+          extraLibraries = pkgs: [];
         })
       ];
       persistence = {
