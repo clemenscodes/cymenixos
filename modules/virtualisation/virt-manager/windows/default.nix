50cbabec75370c60892e2bbde09cfeac1d825718
Clemens Horn <clemens.horn@mni.thm.de>
2025-03-19 11:32:26 +0100

    test

--- modules/virtualisation/virt-manager/windows/default.nix
+++ modules/virtualisation/virt-manager/windows/default.nix
@@ -28,18 +28,18 @@
     name = "qemu";
     text = ''
       set -e
-      GUEST_NAME="$1"
-      HOOK_NAME="$2"
-      STATE_NAME="$3"
-      MISC="''${@:4}"
-      BASEDIR="$(dirname $0)"
-      HOOKPATH="$BASEDIR/qemu.d/$GUEST_NAME/$HOOK_NAME/$STATE_NAME"
+      export GUEST_NAME="$1"
+      export HOOK_NAME="$2"
+      export STATE_NAME="$3"
+      export MISC="''${*:4}"
+      export BASEDIR="$(dirname "$0")"
+      export HOOKPATH="$BASEDIR/qemu.d/$GUEST_NAME/$HOOK_NAME/$STATE_NAME"
       if [ -f "$HOOKPATH" ] && [ -s "$HOOKPATH" ] && [ -x "$HOOKPATH" ]; then
-        eval \"$HOOKPATH\" "$@"
+        eval \""$HOOKPATH"\" "$@"
       elif [ -d "$HOOKPATH" ]; then
-        while read file; do
-          if [ ! -z "$file" ]; then
-            eval \"$file\" "$@"
+        while read -r file; do
+          if [ -n "$file" ]; then
+            eval \""$file"\" "$@"
           fi
         done <<< "$(find -L "$HOOKPATH" -maxdepth 1 -type f -executable -print;)"
       fi
