cdbe5dd5ba6fe034bab61ca139d67b4d07b0d76a
Clemens Horn <clemens.horn@mni.thm.de>
2025-04-06 18:33:58 +0200

    test(obs): gstreamer

--- modules/home-manager/modules/media/video/obs/default.nix
+++ modules/home-manager/modules/media/video/obs/default.nix
@@ -28,6 +28,15 @@ in {
           directories = [".config/obs-studio"];
         };
       };
+      packages = with pkgs; [
+        gst_all_1.gstreamer
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-ugly
+        gst_all_1.gst-libav
+        gst_all_1.gst-vaapi
+      ];
     };
     programs = {
       obs-studio = {
