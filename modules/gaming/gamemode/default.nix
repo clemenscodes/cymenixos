fb0368b766df6384d81908b73b1f48e7b17d225a
Clemens Horn <clemens.horn@mni.thm.de>
2025-04-08 14:27:01 +0200

    chore(nix): update

--- modules/gaming/gamemode/default.nix
+++ modules/gaming/gamemode/default.nix
@@ -24,8 +24,8 @@ in {
             inhibit_screensaver = 0;
           };
           gpu = {
+            gpu_device = 1;
             apply_gpu_optimisations = "accept-responsibility";
-            gpu_device = 0;
             amd_performance_level = "high";
           };
           custom = {
