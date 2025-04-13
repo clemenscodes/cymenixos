d251d343b90cae666af6016d70aa209b740b95d2
Clemens Horn <clemens.horn@mni.thm.de>
2025-04-13 15:14:13 +0200

    chore(yazi): remove most plugins

--- modules/home-manager/modules/explorer/yazi/default.nix
+++ modules/home-manager/modules/explorer/yazi/default.nix
@@ -4,7 +4,6 @@
   ...
 }: {config, ...}: let
   cfg = config.modules.explorer;
-  plugins = import ./plugins {inherit pkgs;};
 in {
   options = {
     modules = {
@@ -45,97 +44,18 @@ in {
             show_hidden = true;
             show_symlink = false;
           };
-          plugin = {
-            prepend_previewers = [
-              {
-                name = "*.md";
-                run = "glow";
-              }
-              {
-                mime = "text/csv";
-                run = "miller";
-              }
-              {
-                mime = "audio/*";
-                run = "exifaudio";
-              }
-              {
-                name = "*.bin";
-                run = "hexyl";
-              }
-            ];
-            append_previewers = [
-              {
-                name = "*";
-                run = "hexyl";
-              }
-            ];
-            prepend_fetchers = [
-              {
-                id = "git";
-                name = "*";
-                run = "git";
-              }
-              {
-                id = "git";
-                name = "*/";
-                run = "git";
-              }
-            ];
-          };
         };
         keymap = {
           manager = {
             prepend_keymap = [
-              {
-                on = ["!"];
-                run = "shell $SHELL --block";
-                desc = "Open shell here";
-              }
               {
                 on = ["l"];
                 run = "plugin smart-enter";
                 desc = "Enter the child directory, or open the file";
               }
               {
-                on = ["p"];
-                run = "plugin smart-paste";
-                desc = "Paste into the hovered directory or CWD";
-              }
-              {
-                on = ["t"];
-                run = "plugin smart-tab";
-                desc = "Create a tab and enter the hovered directory";
-              }
-              {
-                on = ["2"];
-                run = "plugin smart-switch '1'";
-                desc = "Switch or create tab 2";
-              }
-              {
-                on = ["<C-n>"];
-                run = "shell '${pkgs.ripdrag}/bin/ripdrag $@ -x 2>/dev/null &' --confirm";
-              }
-              {
-                on = ["g" "r"];
-                run = "shell '${pkgs.yazi}/bin/ya emit cd $(git rev-parse --show-toplevel)'";
-              }
-              {
-                on = ["k"];
-                run = "plugin arrow '-1'";
-              }
-              {
-                on = ["j"];
-                run = "plugin arrow '1'";
-              }
-              {
                 on = ["<C-y>"];
                 run = "plugin wl-clipboard";
-              }
-              {
-                on = ["E"];
-                run = "plugin eza-preview";
-                desc = "Toggle tree/list dir preview";
               }
               {
                 on = ["d"];
@@ -144,20 +64,7 @@ in {
               }
             ];
           };
-          input = {
-            prepend_keymap = [
-              {
-                on = ["<Esc>"];
-                run = "close";
-                desc = "Cancel input";
-              }
-            ];
-          };
         };
-        initLua = ''
-          require("starship"):setup()
-        '';
-        inherit plugins;
       };
     };
   };
--- modules/home-manager/modules/explorer/yazi/plugins/starship.nix
+++
@@ -1,7 +0,0 @@
-{pkgs, ...}:
-pkgs.fetchFromGitHub {
-  owner = "Rolv-Apneseth";
-  repo = "starship.yazi";
-  rev = "d1cd0a38aa6a2c2e86e62a466f43e415f781031e";
-  hash = "sha256-XiEsykudwYmwSNDO41b5layP1DqVa89e6Emv9Qf0mz0=";
-}
