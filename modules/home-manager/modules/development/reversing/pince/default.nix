9af17c2098108cbb12ce34c6471aadc1e68ccc9c
Clemens Horn <clemens.horn@mni.thm.de>
2025-06-03 21:34:59 +0200

    pince desktop entry with sudo

--- modules/home-manager/modules/development/reversing/pince/default.nix
+++ modules/home-manager/modules/development/reversing/pince/default.nix
@@ -29,6 +29,21 @@ in {
     };
   };
   config = lib.mkIf (cfg.enable && cfg.pince.enable) {
+    xdg = {
+      desktopEntries = {
+        gparted = {
+          name = "PINCE";
+          genericName = "PINCE is not Cheat Engine";
+          comment = "Reverse engineering tool for linux games";
+          type = "Application";
+          exec = "sudo -E ${pkgs.pince}/bin/pince %f";
+          terminal = false;
+          icon = "${pkgs.pince}/share/pince/media/logo/ozgurozbek/pince_big_red.png";
+          categories = ["GTK" "System"];
+          startupNotify = true;
+        };
+      };
+    };
     home = {
       packages = [
         pkgs.pince
