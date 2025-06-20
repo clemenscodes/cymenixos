865749ef2c2835295dc4210be830abb345cdd25c
Clemens Horn <clemens.horn@nexobility.de>
2025-06-20 07:27:21 +0200

    chore(nix): update
    Signed-off-by: Clemens Horn <clemens.horn@nexobility.de>

--- modules/gpu/amd/default.nix
+++ modules/gpu/amd/default.nix
@@ -25,6 +25,18 @@ in {
     };
   };
   config = lib.mkIf (cfg.enable && cfg.amd.enable) {
+    environment = {
+      variables = {
+        OCL_ICD_VENDORS = "${pkgs.rocmPackages.clr.icd}/etc/OpenCL/vendors/";
+        __EGL_VENDOR_LIBRARY_DIRS = "/run/opengl-driver/share/glvnd/egl_vendor.d:/run/opengl-driver-32/share/glvnd/egl_vendor.d";
+        LIBVA_DRIVERS_PATH = "/run/opengl-driver/lib/dri:/run/opengl-driver-32/lib/dri";
+        VDPAU_DRIVER_PATH = "/run/opengl-driver/lib/vdpau:/run/opengl-driver-32/lib/vdpau";
+        VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/radeon_icd.i686.json";
+        VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/radeon_icd.i686.json";
+        VDPAU_DRIVER = "radeonsi";
+        LIBVA_DRIVER_NAME = "radeonsi";
+      };
+    };
     modules = {
       gpu = {
         vendor = "amd";
