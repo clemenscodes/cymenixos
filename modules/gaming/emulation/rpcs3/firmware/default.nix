d744c3fe11cc3c092d7d899a1eb55d9203bf3ea2
Clemens Horn <clemens.horn@mni.thm.de>
2025-06-21 09:42:17 +0200

    update firmware rpcs3

--- modules/gaming/emulation/rpcs3/firmware/default.nix
+++ modules/gaming/emulation/rpcs3/firmware/default.nix
@@ -3,8 +3,8 @@ with pkgs;
   stdenv.mkDerivation {
     name = "ps3bios";
     src = fetchurl {
-      url = "http://dus01.ps3.update.playstation.net/update/ps3/image/us/2024_0227_3694eb3fb8d9915c112e6ab41a60c69f/PS3UPDAT.PUP";
-      sha256 = "1xw00fns2cvy75czq2z4pz2n45da5db7g98ybmrrdd9nj22bv55w";
+      url = "http://dus01.ps3.update.playstation.net/update/ps3/image/us/2025_0305_c179ad173bbc08b55431d30947725a4b/PS3UPDAT.PUP";
+      sha256 = "0r6lr7ify6g7kbqgfah1d4dknllkz4hpvsp4zs7k7l4h68ll9h4r";
     };
     phases = "installPhase";
     installPhase = ''
