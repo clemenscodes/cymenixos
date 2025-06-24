be4f26a4aa67b3ae4b53c9d5ec1ec38ec2426acd
Clemens Horn <clemens.horn@mni.thm.de>
2025-06-24 11:37:23 +0200

    revert memlock

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -31,19 +31,13 @@
         loginLimits = [
           {
             domain = "*";
-            type = "hard";
-            item = "memlock";
-            value = "unlimited";
-          }
-          {
-            domain = "*";
-            type = "soft";
-            item = "memlock";
-            value = "unlimited";
-          }
-          {
-            domain = "*";
             item = "nofile";
+            type = "-";
+            value = "524288";
+          }
+          {
+            domain = "*";
+            item = "memlock";
             type = "-";
             value = "524288";
           }
