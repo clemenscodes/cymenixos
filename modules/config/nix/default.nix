7b8fab6dbe44a80e4e3e236ad60a7b6cff01136c
Clemens Horn <clemens.horn@mni.thm.de>
2025-07-15 19:21:23 +0200

    chore(nix): update

--- modules/config/nix/default.nix
+++ modules/config/nix/default.nix
@@ -50,8 +50,14 @@ in {
         keep-outputs = true;
         allowed-users = ["@wheel"];
         trusted-users = ["@wheel"];
-        substituters = lib.mkIf (!config.modules.airgap.enable) ["https://nix-community.cachix.org"];
-        trusted-public-keys = lib.mkIf (!config.modules.airgap.enable) ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+        substituters = lib.mkIf (!config.modules.airgap.enable) [
+          "https://nix-community.cachix.org"
+          "https://chaotic-nyx.cachix.org/"
+        ];
+        trusted-public-keys = lib.mkIf (!config.modules.airgap.enable) [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        ];
         experimental-features = ["nix-command" "flakes" "fetch-closure"];
       };
       gc = lib.mkIf (!config.modules.airgap.enable) {
