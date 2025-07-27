3555078e767238816867c6ba19f6917007575880
Clemens Horn <clemens.horn@mni.thm.de>
2025-07-27 20:10:51 +0200

    test

--- modules/virtualisation/virt-manager/windows/default.nix
+++ modules/virtualisation/virt-manager/windows/default.nix
@@ -71,7 +71,7 @@
       GUEST_PORT="5900"
       HOST_PORT="5900"
       if [ "$1" = "win11" ]; then
-        iptables -A FORWARD -s 192.168.178.140/24 -d 192.168.122.0/24 -o virbr0 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
+        iptables -A FORWARD -s 192.168.178.137/24 -d 192.168.122.0/24 -o virbr0 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
         if [ "$2" = "stopped" ] || [ "$2" = "reconnect" ]; then
          iptables -D FORWARD -o virbr0 -p tcp -d "$GUEST_IP" --dport "$GUEST_PORT" -j ACCEPT
          iptables -t nat -D PREROUTING -p tcp --dport "$HOST_PORT" -j DNAT --to "$GUEST_IP:$GUEST_PORT"
@@ -651,7 +651,14 @@ in {
                         type = "spice";
                         autoport = true;
                         listen = {
-                          type = "none";
+                          type = "address";
+                          address = "127.0.0.1";
+                        };
+                        image = {
+                          compression = false;
+                        };
+                        gl = {
+                          enable = false;
                         };
                       };
                       sound = {
