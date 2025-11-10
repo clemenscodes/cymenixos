9994f0401d460d99990280f1fbaeed199537ce2f
Clemens Horn <clemens.horn@mni.thm.de>
2025-11-10 11:26:55 +0100

    add pnpm caching module

--- modules/home-manager/modules/development/default.nix
+++ modules/home-manager/modules/development/default.nix
@@ -9,6 +9,7 @@
     (import ./gh {inherit inputs pkgs lib;})
     (import ./git {inherit inputs pkgs lib;})
     (import ./pentesting {inherit inputs pkgs lib;})
+    (import ./pnpm {inherit inputs pkgs lib;})
     (import ./postman {inherit inputs pkgs lib;})
     (import ./reversing {inherit inputs pkgs lib;})
     (import ./tongo {inherit inputs pkgs lib;})
