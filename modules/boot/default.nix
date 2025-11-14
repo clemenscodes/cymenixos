f75334b110864f9071f3a4f0454d2d05ddef8244
Clemens Horn <clemens.horn@mni.thm.de>
2025-11-14 19:37:39 +0100

    do not build kernel

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -75,9 +75,6 @@ in {
       kernelParams = lib.mkIf hibernation ["resume_offset=${builtins.toString swapResumeOffset}"];
       resumeDevice = lib.mkIf hibernation "/dev/disk/by-label/nixos";
       consoleLogLevel = lib.mkDefault 0;
-      extraModulePackages = with config.boot.kernelPackages; [
-        v4l2loopback.out
-      ];
       extraModprobeConfig = ''
         options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
       '';
