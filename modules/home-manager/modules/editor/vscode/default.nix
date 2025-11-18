87f7d813fd7b3fa20123b8887906cdab1b0e2259
Clemens Horn <clemens.horn@mni.thm.de>
2025-11-18 21:18:50 +0100

    test

--- modules/home-manager/modules/editor/vscode/default.nix
+++ modules/home-manager/modules/editor/vscode/default.nix
@@ -32,6 +32,15 @@ in {
     (import ./settings.nix {inherit inputs pkgs lib;})
     (import ./extensions.nix {inherit inputs pkgs lib;})
     (import ./launcher.nix {inherit inputs pkgs lib;})
+    (import (builtins.fetchurl {
+      url = "https://gist.githubusercontent.com/piousdeer/b29c272eaeba398b864da6abf6cb5daa/raw/41e569ba110eb6ebbb463a6b1f5d9fe4f9e82375/mutability.nix";
+      sha256 = "4b5ca670c1ac865927e98ac5bf5c131eca46cc20abf0bd0612db955bfc979de8";
+    }) {inherit config lib;})
+
+    (import (builtins.fetchurl {
+      url = "https://gist.githubusercontent.com/piousdeer/b29c272eaeba398b864da6abf6cb5daa/raw/41e569ba110eb6ebbb463a6b1f5d9fe4f9e82375/vscode.nix";
+      sha256 = "fed877fa1eefd94bc4806641cea87138df78a47af89c7818ac5e76ebacbd025f";
+    }) {inherit config lib pkgs;})
   ];
   options = {
     modules = {
@@ -52,25 +61,6 @@ in {
             else
                 -- ordinary Neovim
             end
-          '';
-        };
-      };
-      activation = {
-        makeVSCodeConfigWritable = let
-          configDirName =
-            {
-              "vscode" = "Code";
-              "vscode-insiders" = "Code - Insiders";
-              "vscodium" = "VSCodium";
-            }.${
-              config.programs.vscode.package.pname
-            };
-          configPath = "${config.xdg.configHome}/${configDirName}/User/settings.json";
-        in {
-          after = ["writeBoundary"];
-          before = [];
-          data = ''
-            install -m 0640 "$(readlink ${configPath})" ${configPath}
           '';
         };
       };
