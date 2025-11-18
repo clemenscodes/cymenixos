7388df01b4af8bb6f048fd66509ddd0e39e1b2d6
Clemens Horn <clemens.horn@mni.thm.de>
2025-11-18 21:34:10 +0100

    test

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -9,15 +9,36 @@
   ...
 }: let
   cfg = config.modules;
+  fileOptionAttrPaths = [["home" "file"] ["xdg" "configFile"] ["xdg" "dataFile"]];
+  mergeAttrsList = builtins.foldl' (lib.mergeAttrs) {};
+  fileAttrsType = lib.types.attrsOf (lib.types.submodule ({config, ...}: {
+    options.mutable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = ''
+        Whether to copy the file without the read-only attribute instead of
+        symlinking. If you set this to `true`, you must also set `force` to
+        `true`. Mutable files are not removed when you remove them from your
+        configuration.
+        This option is useful for programs that don't have a very good
+        support for read-only configurations.
+      '';
+    };
+  }));
 in {
   imports = [inputs.home-manager.nixosModules.home-manager];
-  options = {
-    modules = {
-      home-manager = {
-        enable = lib.mkEnableOption "Enable home-manager" // {default = false;};
+  options =
+    {
+      modules = {
+        home-manager = {
+          enable = lib.mkEnableOption "Enable home-manager" // {default = false;};
+        };
       };
-    };
-  };
+    }
+    // mergeAttrsList (map (attrPath:
+      lib.setAttrByPath attrPath (lib.mkOption {type = fileAttrsType;}))
+    fileOptionAttrPaths);
+
   config = {
     system = {
       stateVersion = lib.mkDefault (lib.versions.majorMinor lib.version);
@@ -34,6 +55,30 @@ in {
             stateVersion = lib.mkDefault (lib.versions.majorMinor lib.version);
           };
         };
+      };
+      activation = {
+        mutableFileGeneration = let
+          allFiles = builtins.concatLists (map
+            (attrPath: builtins.attrValues (lib.getAttrFromPath attrPath config))
+            fileOptionAttrPaths);
+          filterMutableFiles = builtins.filter (file:
+            (file.mutable or false)
+            && lib.assertMsg file.force
+            "if you specify `mutable` to `true` on a file, you must also set `force` to `true`");
+          mutableFiles = filterMutableFiles allFiles;
+          toCommand = file: let
+            source = lib.escapeShellArg file.source;
+            target = lib.escapeShellArg file.target;
+          in ''
+            $VERBOSE_ECHO "${source} -> ${target}"
+            $DRY_RUN_CMD cp --remove-destination --no-preserve=mode ${source} ${target}
+          '';
+          command =
+            ''
+              echo "Copying mutable home files for $HOME"
+            ''
+            + lib.concatLines (map toCommand mutableFiles);
+        in (inputs.home-manager.lib.hm.dag.entryAfter ["linkGeneration"] command);
       };
     };
   };
--- modules/home-manager/modules/editor/vscode/default.nix
+++ modules/home-manager/modules/editor/vscode/default.nix
@@ -56,10 +56,6 @@ in {
     (import ./settings.nix {inherit inputs pkgs lib;})
     (import ./extensions.nix {inherit inputs pkgs lib;})
     (import ./launcher.nix {inherit inputs pkgs lib;})
-    (import (builtins.fetchurl {
-      url = "https://gist.githubusercontent.com/piousdeer/b29c272eaeba398b864da6abf6cb5daa/raw/41e569ba110eb6ebbb463a6b1f5d9fe4f9e82375/mutability.nix";
-      sha256 = "4b5ca670c1ac865927e98ac5bf5c131eca46cc20abf0bd0612db955bfc979de8";
-    }) {inherit config lib;})
   ];
   options = {
     modules = {
