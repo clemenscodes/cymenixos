21b4626b90f269712a2aa68163688e50356abd04
Clemens Horn <clemens.horn@mni.thm.de>
2025-11-18 21:26:03 +0100

    test

--- modules/home-manager/modules/editor/vscode/default.nix
+++ modules/home-manager/modules/editor/vscode/default.nix
@@ -26,6 +26,34 @@
     nix run github:clemenscodes/codevim -- "$@"
   '';
   cfg = config.modules.editor;
+
+  vscodeCfg = config.programs.vscode;
+
+  vscodePname = vscodeCfg.package.pname;
+
+  configDir =
+    {
+      "vscode" = "Code";
+      "vscode-insiders" = "Code - Insiders";
+      "vscodium" = "VSCodium";
+    }.${
+      vscodePname
+    };
+
+  userDir = "${config.xdg.configHome}/${configDir}/User";
+  configFilePath = "${userDir}/settings.json";
+  tasksFilePath = "${userDir}/tasks.json";
+  keybindingsFilePath = "${userDir}/keybindings.json";
+  snippetDir = "${userDir}/snippets";
+  pathsToMakeWritable = lib.flatten [
+    (lib.optional (cfg.profiles.default.userTasks != {}) tasksFilePath)
+    (lib.optional (cfg.profiles.default.userSettings != {}) configFilePath)
+    (lib.optional (cfg.profiles.default.keybindings != []) keybindingsFilePath)
+    (lib.optional (cfg.profiles.default.globalSnippets != {})
+      "${snippetDir}/global.code-snippets")
+    (lib.mapAttrsToList (language: _: "${snippetDir}/${language}.json")
+      cfg.profiles.default.languageSnippets)
+  ];
 in {
   imports = [
     (import ./keybindings.nix {inherit inputs pkgs lib;})
@@ -36,11 +64,6 @@ in {
       url = "https://gist.githubusercontent.com/piousdeer/b29c272eaeba398b864da6abf6cb5daa/raw/41e569ba110eb6ebbb463a6b1f5d9fe4f9e82375/mutability.nix";
       sha256 = "4b5ca670c1ac865927e98ac5bf5c131eca46cc20abf0bd0612db955bfc979de8";
     }) {inherit config lib;})
-
-    (import (builtins.fetchurl {
-      url = "https://gist.githubusercontent.com/piousdeer/b29c272eaeba398b864da6abf6cb5daa/raw/41e569ba110eb6ebbb463a6b1f5d9fe4f9e82375/vscode.nix";
-      sha256 = "fed877fa1eefd94bc4806641cea87138df78a47af89c7818ac5e76ebacbd025f";
-    }) {inherit config lib pkgs;})
   ];
   options = {
     modules = {
@@ -53,17 +76,22 @@ in {
   };
   config = lib.mkIf (cfg.enable && cfg.vscode.enable) {
     home = {
-      file = {
-        ".config/nvim/init.vscode.lua" = {
-          text = ''
-            if vim.g.vscode then
-                -- VSCode extension
-            else
-                -- ordinary Neovim
-            end
-          '';
+      file =
+        lib.genAttrs pathsToMakeWritable (_: {
+          force = true;
+          mutable = true;
+        })
+        // {
+          ".config/nvim/init.vscode.lua" = {
+            text = ''
+              if vim.g.vscode then
+                  -- VSCode extension
+              else
+                  -- ordinary Neovim
+              end
+            '';
+          };
         };
-      };
       packages = [codevim];
       persistence = lib.mkIf osConfig.modules.boot.enable {
         "${osConfig.modules.boot.impermanence.persistPath}${config.home.homeDirectory}" = {
