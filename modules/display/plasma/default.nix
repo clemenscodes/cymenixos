b78fed00b87514d8bd5b947c5773bef03a46b7c3
Clemens Horn <me@clemenshorn.com>
2025-11-24 13:59:18 +0100

    test

--- modules/display/plasma/default.nix
+++ modules/display/plasma/default.nix
@@ -146,9 +146,6 @@ in {
                     evening = "18:00";
                   };
                 };
-                tiling = {
-                  layout = {};
-                };
                 virtualDesktops = {
                   number = 10;
                   names = ["1" "2" "3" "4" "5" "6" "7" "8" "9" "0"];
