2caeb002289e139591ecabb7ca0b670e560695c5
Clemens Horn <me@clemenshorn.com>
2025-12-16 09:06:06 +0100

    use openrgb that build for ornicar

--- modules/rgb/default.nix
+++ modules/rgb/default.nix
@@ -6,6 +6,23 @@
 }: {config, ...}: let
   cfg = config.modules;
   inherit (cfg.rgb) enable;
+  openrgb = pkgs.openrgb.overrideAttrs (previousAttrs: {
+    version = "candidate_1.0rc2-dev";
+    src = pkgs.fetchFromGitHub {
+      owner = "CalcProgrammer1";
+      repo = "OpenRGB";
+      rev = "56b75aaffc730f1e28c77d576d94d70983bb1db7";
+      hash = "sha256-/jbwP8urk0wgj3KCGuUSwJfbKqSV9GQO/dc5d3ZJiT0=";
+    };
+    patches = [];
+    postPatch = ''
+      patchShebangs scripts/build-udev-rules.sh
+      substituteInPlace scripts/build-udev-rules.sh \
+        --replace /bin/chmod "${pkgs.coreutils}/bin/chmod"
+      substituteInPlace scripts/build-udev-rules.sh \
+        --replace /usr/bin/env "${pkgs.coreutils}/bin/env"
+    '';
+  });
 in {
   options = {
     modules = {
@@ -19,12 +36,12 @@ in {
       hardware = {
         openrgb = {
           inherit enable;
-          package = pkgs.openrgb-with-all-plugins;
+          package = openrgb;
         };
       };
     };
     environment = {
-      systemPackages = with pkgs; [openrgb-with-all-plugins];
+      systemPackages = [openrgb];
     };
   };
 }
