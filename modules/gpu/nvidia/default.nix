2990464afd9e148d338915f39953f6bc51abc2f0
Clemens Horn <me@clemenshorn.com>
2026-02-14 20:08:04 +0100

    test

--- modules/gpu/nvidia/default.nix
+++ modules/gpu/nvidia/default.nix
@@ -63,15 +63,13 @@ in {
   config = lib.mkIf (cfg.enable && cfg.nvidia.enable) {
     boot = {
       blacklistedKernelModules = ["nouveau"];
-      initrd = {
-        kernelModules = [
-          "nvidia"
-          "nvidiafb"
-          "nvidia-modeset"
-          "nvidia-uvm"
-          "nvidia-drm"
-        ];
-      };
+      kernelModules = [
+        "nvidia"
+        "nvidiafb"
+        "nvidia-modeset"
+        "nvidia-uvm"
+        "nvidia-drm"
+      ];
     };
     environment = {
       systemPackages = [
