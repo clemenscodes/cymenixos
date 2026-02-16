c37cefa85ab03364a82b2a2341b6ddb0bc083808
Clemens Horn <me@clemenshorn.com>
2026-02-16 16:20:08 +0100

    update

--- modules/virtualisation/virt-manager/default.nix
+++ modules/virtualisation/virt-manager/default.nix
@@ -120,17 +120,6 @@
       GUEST_IP="192.168.122.1"
       GUEST_PORT="5900"
       HOST_PORT="5900"
-      if [ "$1" = "nixos" ]; then
-        iptables -A FORWARD -s 192.168.178.135/24 -d 192.168.122.0/24 -o virbr0 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
-        if [ "$2" = "stopped" ] || [ "$2" = "reconnect" ]; then
-         iptables -D FORWARD -o virbr0 -p tcp -d "$GUEST_IP" --dport "$GUEST_PORT" -j ACCEPT
-         iptables -t nat -D PREROUTING -p tcp --dport "$HOST_PORT" -j DNAT --to "$GUEST_IP:$GUEST_PORT"
-        fi
-        if [ "$2" = "start" ] || [ "$2" = "reconnect" ]; then
-         iptables -I FORWARD -o virbr0 -p tcp -d "$GUEST_IP" --dport "$GUEST_PORT" -j ACCEPT
-         iptables -t nat -I PREROUTING -p tcp --dport "$HOST_PORT" -j DNAT --to "$GUEST_IP:$GUEST_PORT"
-        fi
-      fi
       if [ "$1" = "win11" ]; then
         iptables -A FORWARD -s 192.168.178.135/24 -d 192.168.122.0/24 -o virbr0 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
         if [ "$2" = "stopped" ] || [ "$2" = "reconnect" ]; then
