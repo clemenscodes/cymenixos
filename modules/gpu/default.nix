9c0f50ca454135cce106e27f705f53d7ba07eb8c
Clemens Horn <me@clemenshorn.com>
2026-02-21 11:03:09 +0100

    update

--- modules/gpu/default.nix
+++ modules/gpu/default.nix
@@ -22,59 +22,10 @@ in {
     };
   };
   config = {
-    environment = {
-      systemPackages = [
-        pkgs.clinfo
-        pkgs.mesa-demos
-        pkgs.glmark2
-        pkgs.libva
-        pkgs.libva-utils
-        pkgs.vdpauinfo
-        pkgs.vulkan-tools
-        pkgs.vulkan-loader
-        pkgs.vulkan-validation-layers
-        pkgs.vulkan-extension-layer
-      ];
-    };
     hardware = {
       graphics = {
         inherit (cfg.gpu) enable;
         enable32Bit = true;
-        extraPackages = [
-          pkgs.mesa.opencl
-          pkgs.intel-media-driver
-          pkgs.rocmPackages.clr
-          pkgs.rocmPackages.clr.icd
-          pkgs.rocmPackages.rocm-runtime
-          pkgs.libdrm
-          pkgs.libva
-          pkgs.libva-vdpau-driver
-          pkgs.libvdpau-va-gl
-          pkgs.intel-vaapi-driver
-          pkgs.vkd3d
-          pkgs.vkd3d-proton
-        ];
-        extraPackages32 = [
-          pkgs.pkgsi686Linux.mesa.opencl
-          pkgs.pkgsi686Linux.intel-media-driver
-          pkgs.pkgsi686Linux.intel-vaapi-driver
-        ];
-      };
-    };
-    systemd = {
-      tmpfiles = {
-        rules = let
-          rocmEnv = pkgs.symlinkJoin {
-            name = "rocm-combined";
-            paths = with pkgs.rocmPackages; [
-              rocblas
-              hipblas
-              clr
-            ];
-          };
-        in [
-          "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
-        ];
       };
     };
   };
