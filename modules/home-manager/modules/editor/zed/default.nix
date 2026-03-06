285abd8a6242f6f3674bbee0a3df1328971fc4e5
Clemens Horn <me@clemenshorn.com>
2026-03-06 07:52:42 +0100

    update

--- modules/home-manager/modules/editor/zed/default.nix
+++ modules/home-manager/modules/editor/zed/default.nix
@@ -353,39 +353,6 @@ in {
               binary = {
                 path_lookup = true;
               };
-              initialization_options = {
-                check = {
-                  command = "clippy";
-                };
-                cargo = {
-                  allFeatures = true;
-                  loadOutDirsFromCheck = true;
-                  buildScripts = {
-                    enable = true;
-                  };
-                };
-                procMacro = {
-                  enable = true;
-                  ignored = {
-                    async-trait = ["async_trait"];
-                    napi-derive = ["napi"];
-                    async-recursion = ["async_recursion"];
-                  };
-                };
-                rust = {
-                  analyzerTargetDir = true;
-                };
-                inlayHints = {
-                  maxLength = null;
-                  lifetimeElisionHints = {
-                    enable = "skip_trivial";
-                    useParameterNames = true;
-                  };
-                  closureReturnTypeHints = {
-                    enable = "always";
-                  };
-                };
-              };
             };
             nix = {
               binary = {
