e32215e9c7d9b8b5f1cd5b4f3f954b6c8945ca59
Clemens Horn <me@clemenshorn.com>
2026-03-10 20:50:50 +0100

    test

--- modules/virtualisation/virt-manager/default.nix
+++ modules/virtualisation/virt-manager/default.nix
@@ -245,8 +245,6 @@ in {
       services = {
         libvirtd = {
           preStart = ''
-            mkdir -p /var/lib/libvirt/vgabios
-            ln -sf ${qemu}/bin/qemu /var/lib/libvirt/hooks/qemu
             ${lib.getExe qemu-mkdisk-win}
             ${lib.getExe qemu-mkdisk-nixos}
           '';
