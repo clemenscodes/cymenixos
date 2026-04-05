e606177802fa928bd25caff79aa1033943a25c4a
Clemens Horn <me@clemenshorn.com>
2026-04-05 11:57:30 +0200

    update

--- modules/io/wooting/default.nix
+++ modules/io/wooting/default.nix
@@ -14,6 +14,45 @@
       allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) ["wootility"];
     };
   };
+  wootilityVersion = "5.2.5";
+  wootilitySrc = pkgs.fetchurl {
+    url = "https://wootility-updates.ams3.cdn.digitaloceanspaces.com/wootility-linux/Wootility-${wootilityVersion}.AppImage";
+    hash = "sha256-bDhwlI+zi13xdMXqT5ztzR7RNOLgTBxNtjGkEFezZsw=";
+  };
+  wootilityContents = pkgs.appimageTools.extract {
+    pname = "wootility";
+    version = wootilityVersion;
+    src = wootilitySrc;
+  };
+  wootility = pkgs.appimageTools.wrapType2 {
+    pname = "wootility";
+    version = wootilityVersion;
+    src = wootilitySrc;
+    nativeBuildInputs = [pkgs.makeWrapper];
+    extraInstallCommands = ''
+      wrapProgram $out/bin/wootility \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
+
+      install -Dm444 ${wootilityContents}/wootility.desktop -t $out/share/applications
+      substituteInPlace $out/share/applications/wootility.desktop \
+        --replace-fail 'Exec=AppRun --no-sandbox' 'Exec=wootility'
+
+      for size in 16x16 32x32 48x48 64x64 128x128 256x256 512x512 1024x1024; do
+        if [ -f ${wootilityContents}/usr/share/icons/hicolor/$size/apps/wootility.png ]; then
+          install -Dm444 ${wootilityContents}/usr/share/icons/hicolor/$size/apps/wootility.png \
+            $out/share/icons/hicolor/$size/apps/wootility.png
+        fi
+      done
+    '';
+    profile = ''
+      export LC_ALL=C.UTF-8
+    '';
+    extraPkgs = epkgs:
+      with epkgs; [
+        libxkbfile
+      ];
+    meta = pkgs.wootility.meta;
+  };
 in {
   options = {
     modules = {
@@ -29,7 +68,7 @@ in {
     };
   };
   config = lib.mkIf (cfg.enable && cfg.wooting.enable) {
-    environment.systemPackages = [pkgs.wootility];
+    environment.systemPackages = [wootility];
     services.udev.packages = [pkgs.wooting-udev-rules];
   };
 }
