6f0606db8d09794754cb54b302e13f4ce0722ccb
Clemens <clemenscodes@gmail.com>
2026-06-26 13:12:26 +0200

    pin nvidia driver to 595.84

--- modules/gpu/nvidia/default.nix
+++ modules/gpu/nvidia/default.nix
@@ -90,7 +90,13 @@ in {
           enable = false;
           finegrained = false;
         };
-        package = config.boot.kernelPackages.nvidiaPackages.beta;
+        package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "595.84";
+          sha256_64bit = "sha256-mcQE5SExvye8ptoCaNzOPr7cenOrF0BxqZXPGmxeugY=";
+          openSha256 = "sha256-pEmA2tUcOKwUPKy6N0QvS49Pdut4/7Phs/JhjdyBcNY=";
+          settingsSha256 = "sha256-QrnBM+sdWO4GanO62rxpHmRrjYkYpl5RD6fIiHq4C4A=";
+          persistencedSha256 = "sha256-50xYdgx7EEThbaMp4QS8GADbxj0mhBXh8QQN0tWMwRg=";
+        };
         open = true;
         nvidiaSettings = true;
         nvidiaPersistenced = false;
